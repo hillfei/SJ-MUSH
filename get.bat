0cb9b4b9a8a3f0a5e080ffaa9366878244400711
Kos Hill <kos.hill@gmail.com>
2017-09-06 10:54:21 +0800

    Add more

---
+++ get.bat
@@ -0,0 +1,9 @@
+echo -------pack---------------   
+c:
+cd  c:\MUSHS
+"c:\Program Files\WinRAR\WinRAR.exe" a -r c:\GitHub\SJ-MUSH\MUSHS.rar *.lua   
+"C:\Program Files (x86)\WinRAR\WinRAR.exe" a -r c:\GitHub\SJ-MUSH\MUSHS.rar *.lua   
+cd c:\GitHub\SJ-MUSH
+"c:\Program Files\WinRAR\WinRAR.exe" x c:\GitHub\SJ-MUSH\MUSHS.rar .\  
+"C:\Program Files (x86)\WinRAR\WinRAR.exe" x c:\GitHub\SJ-MUSH\MUSHS.rar .\  
+echo --------------------------  
