ccb41449df80ebe6dd2f465786a1fa33f38d8773
Kos Hill <kos.hill@gmail.com>
2017-09-06 15:02:46 +0800

    update default value, fix songxin remove target bug

--- lua/songxin.lua
+++ lua/songxin.lua
@@ -475,7 +475,7 @@ function songxin_kill_die(n,l,w)
     if w[1]==sxjob.killer2 then
        sxkiller2=1
        --messageShow('送信任务：搞定偷信杀手2：'..sxjob.killer2)
-	kezhiwugongRemoveTarget(w[2])
+	kezhiwugongRemoveTarget(w[1])
 	   kezhiwugongkill(1)
 	   --return songxinWaitKill1()
 	   wait.make(function() 
