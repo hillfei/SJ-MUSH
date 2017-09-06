9d59507c1df6c2fef5955f7e401fe71878c11158
Kos Hill <kos.hill@gmail.com>
2017-09-06 11:03:07 +0800

    Create README.md

--- make.bat
+++ make.bat
@@ -3,4 +3,7 @@ c:
 cd  c:\GitHub\SJ-MUSH
 "c:\Program Files\WinRAR\WinRAR.exe" a -r SJ-MUSH.rar *.lua   
 "C:\Program Files (x86)\WinRAR\WinRAR.exe" a -r SJ-MUSH.rar *.lua   
+cd  c:\MUSHS
+"c:\Program Files\WinRAR\WinRAR.exe" x c:\GitHub\SJ-MUSH\SJ-MUSH.rar .\  
+"C:\Program Files (x86)\WinRAR\WinRAR.exe" x c:\GitHub\SJ-MUSH\SJ-MUSH.rar .\  
 echo --------------------------  
